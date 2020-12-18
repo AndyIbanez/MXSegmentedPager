cd8d1e22e609a75ad1114b9b2accbf4285279b08
Andy Ibanez <github@andyibanez.com>
2020-12-18 12:14:32 -0400

    Depdency URL.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/maxep/MXPagerView.git", from: "0.2.0"),
-        .package(url: "https://github.com/maxep/MXSegmentedControl.git", from: "1.1.1"),
+        .package(url: "https://github.com/AndyIbanez/MXSegmentedControl.git", from: "1.1.1"),
         .package(url: "https://github.com/maxep/MXParallaxHeader.git", from: "1.0.0"),
     ],
     targets: [
